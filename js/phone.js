99cb2bff1d9e459969c1a282b774c30f06fbb05b
Rasel Sharif <raselsharif9@gmail.com>
2023-08-27 22:04:25 +0600

    added display phone function

--- js/phone.js
+++ js/phone.js
@@ -1,12 +1,23 @@
-const loadPhone = async() =>{
-    const res = await fetch ('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
     const phones = data.data;
-    console.log(phones);
+    displayPhone(phones);
 }
 
-const displayPhone = phones =>{
+const displayPhone = phones => {
+    const phoneContainer = document.getElementById('phone-container');
 
+    phones.forEach(phone => {
+        const phoneDiv = document.createElement('div');
+        phoneDiv.innerHTML = `
+        <h2>Brand: ${phone.brand}</h2>
+        <h3>Phone: ${phone.phone_name}</h3>
+        <h2>Model: ${phone.slug}</h2>
+        <img src="${phone.image}" alt="phone image">
+        `
+        phoneContainer.appendChild(phoneDiv)
+    })
 }
 
 loadPhone();
